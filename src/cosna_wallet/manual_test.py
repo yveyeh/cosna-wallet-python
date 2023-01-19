c21904a93e2248f8b0f0f59fa58ceb8194333ec3
Didier Yufenyuy Veyeh <yveyeh@gmail.com>
2023-01-19 12:46:36 +0100

    python sdk source code

---
+++ src/cosna_wallet/manual_test.py
@@ -0,0 +1,15 @@
+# from cosna_wallet import CosnaWallet
+
+# wallet: CosnaWallet = CosnaWallet('master_key_yTh7mLB5eB4C1PbMOn3h348HfAAv55Y9', 'private_key_6JwbGpw6ZbdM9PRoCNhEPbH7Jm6yTP7i')
+
+# response = wallet.init_payment({
+#         "amount": 300,
+#         "currency": "XAF",
+#         "hash": "85fd974423b56c15ef2d7ad6e25d9b98",
+#         "return_url": "https://www.fb.com",
+#         "cancel_url": "https://www.yt.com",
+#         "callback_url": "https://beta.cosna-afrique.com"
+#     }
+# )
+
+# print(response.text)
